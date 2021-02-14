5f40d731f5c705e79d3e1d988db71904c47569bc
Przemyslaw Wozniak <inz.przemyslaw.wozniak@gmail.com>
2021-02-14 11:37:25 +0100

    Implemented test exercises

--- src/test/java/pl/sda/zdjavapol73/ProductRatingTest.java
+++ src/test/java/pl/sda/zdjavapol73/ProductRatingTest.java
@@ -33,4 +33,64 @@ public class ProductRatingTest {
         Assertions.assertThat(pr.getScore()).isEqualTo(5);
     }
 
+    @Test
+    void should_set_score_to_1_when_given_1() {
+        //given
+        ProductRating pr = new ProductRating();
+
+        //when
+        pr.setScore(1);
+
+        //then
+        Assertions.assertThat(pr.getScore()).isEqualTo(1);
+    }
+
+    @Test
+    void should_set_score_to_10_when_given_10() {
+        //given
+        ProductRating pr = new ProductRating();
+
+        //when
+        pr.setScore(10);
+
+        //then
+        Assertions.assertThat(pr.getScore()).isEqualTo(10);
+    }
+
+    @Test
+    void should_set_score_to_10_when_given_11() {
+        //given
+        ProductRating pr = new ProductRating();
+
+        //when
+        pr.setScore(11);
+
+        //then
+        Assertions.assertThat(pr.getScore()).isEqualTo(10);
+    }
+
+    @Test
+    void should_set_score_to_1_when_given_0() {
+        //given
+        ProductRating pr = new ProductRating();
+
+        //when
+        pr.setScore(0);
+
+        //then
+        Assertions.assertThat(pr.getScore()).isEqualTo(1);
+    }
+
+    @Test
+    void should_set_score_to_1_when_given_minus10() {
+        //given
+        ProductRating pr = new ProductRating();
+
+        //when
+        pr.setScore(-10);
+
+        //then
+        Assertions.assertThat(pr.getScore()).isEqualTo(1);
+    }
+
 }
