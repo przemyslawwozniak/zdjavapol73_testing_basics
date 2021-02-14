ca85b89e7622a645d6fdc52072c97540a3d4a11d
Przemyslaw Wozniak <inz.przemyslaw.wozniak@gmail.com>
2021-02-14 11:20:36 +0100

    Example solution

--- src/test/java/pl/sda/zdjavapol73/ProductRatingTest.java
+++ src/test/java/pl/sda/zdjavapol73/ProductRatingTest.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.Test;
 public class ProductRatingTest {
 
     @Test
-    public void should_initialize_product_rating_score() {  //shouldInitializeProductRatingScore
+    void should_initialize_product_rating_score() {  //shouldInitializeProductRatingScore
         ProductRating pr = new ProductRating();
 
         //Assertions.assertEquals(0, pr.getScore());    //JUnit 5, latwo pomylic kolejnosc argumentow
@@ -14,12 +14,23 @@ public class ProductRatingTest {
     }
 
     /*
-    1. setScore(5) powoduje ustawienie ProductRating.score = 5
     2. setScore(1)	//wartosci graniczne
     3. setScore(10)	//wartosci graniczne
     4. setScore(11) -> 10 //wyjscie poza zakres - nieprawidlowe wartosci
     5. setScore(0) -> 1
     6. setScore(-10) -> 1
      */
+
+    @Test
+    void should_set_score_to_5_when_given_5() {
+        //given
+        ProductRating pr = new ProductRating();
+
+        //when
+        pr.setScore(5);
+
+        //then
+        Assertions.assertThat(pr.getScore()).isEqualTo(5);
+    }
 
 }
