66de388a30fc695b9323d7990f03e3102e4d9bce
Przemyslaw Wozniak <inz.przemyslaw.wozniak@gmail.com>
2021-02-14 10:56:41 +0100

    Included AssertJ + exercise

--- src/test/java/pl/sda/zdjavapol73/ProductRatingTest.java
+++ src/test/java/pl/sda/zdjavapol73/ProductRatingTest.java
@@ -1,6 +1,6 @@
 package pl.sda.zdjavapol73;
 
-import org.junit.jupiter.api.Assertions;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class ProductRatingTest {
@@ -9,7 +9,17 @@ public class ProductRatingTest {
     public void should_initialize_product_rating_score() {  //shouldInitializeProductRatingScore
         ProductRating pr = new ProductRating();
 
-        Assertions.assertTrue(pr.getScore() == 0);
+        //Assertions.assertEquals(0, pr.getScore());    //JUnit 5, latwo pomylic kolejnosc argumentow
+        Assertions.assertThat(pr.getScore()).isEqualTo(0);  //AssertJ - czytelniejszy
     }
+
+    /*
+    1. setScore(5) powoduje ustawienie ProductRating.score = 5
+    2. setScore(1)	//wartosci graniczne
+    3. setScore(10)	//wartosci graniczne
+    4. setScore(11) -> 10 //wyjscie poza zakres - nieprawidlowe wartosci
+    5. setScore(0) -> 1
+    6. setScore(-10) -> 1
+     */
 
 }
